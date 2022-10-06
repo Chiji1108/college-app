2ceffe805ab94c79efe7bd97f14959db28c18e3e
千々岩 / Chijiiwa <shingo.pad@gmail.com>
2022-10-06 21:11:28 +0900

    wip

---
+++ src/pages/login.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Login: NextPage = () => {
+  return <h1 className="text-3xl font-bold underline">Login</h1>;
+};
+
+export default Login;
